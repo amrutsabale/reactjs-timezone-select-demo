4e7f8372053b413674608fe973d4a4657beee7ed
amrutsabale <samrut355@gmail.com>
2022-08-28 13:35:45 +0530

    Created demos with storybook

---
+++ src/components/SelectTimezone.js
@@ -0,0 +1,116 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { getTimeZones } from "@vvo/tzdb";
+import { DateTime, SystemZone } from "luxon";
+import Select from "react-select";
+
+/**
+ * Primary UI component for user interaction
+ */
+export const SelectTimezone = ({
+  label,
+  name,
+  value,
+  onChange,
+  isDisabled,
+  isSearchable,
+  defaultToSystemTimezone,
+  containerStyle,
+  labelStyles,
+  selectStyles,
+  optionLabelFormat,
+  ...props
+}) => {
+  const timeZones = getTimeZones({ includeUtc: true });
+  const validTimeZones = timeZones
+    .filter(({ name }) => DateTime.local().setZone(name).isValid)
+    .map((timezone) => {
+      const { name, group } = timezone;
+      let label = name;
+      if (optionLabelFormat) {
+        label = optionLabelFormat(timezone) || name;
+      }
+      return {
+        group,
+        label,
+        value: name,
+      };
+    });
+  const options = validTimeZones.map(({ label, value }) => ({ label, value }));
+
+  const defaultTimezone = validTimeZones.find(({ value: tzValue, group }) => {
+    const systemZone = new SystemZone();
+
+    if (value) {
+      return tzValue === value || group.includes(value);
+    }
+    return tzValue === systemZone.name || group.includes(systemZone.name);
+  });
+
+  const defaultTimezoneOption = defaultTimezone
+    ? { value: defaultTimezone.value, label: defaultTimezone.label }
+    : undefined;
+
+  return (
+    <div style={containerStyle}>
+      {label && (
+        <div
+          style={{
+            textAlign: "start",
+            marginBottom: 10,
+            ...(labelStyles && labelStyles),
+          }}
+        >
+          <label htmlFor={name}>{label}</label>
+        </div>
+      )}
+      <Select
+        id={name}
+        options={options}
+        name={name}
+        value={defaultTimezoneOption}
+        onChange={onChange}
+        isDisabled={isDisabled}
+        isSearchable={isSearchable}
+        styles={selectStyles}
+        {...props}
+      />
+    </div>
+  );
+};
+
+SelectTimezone.propTypes = {
+  /**
+   * SelectTimezone name
+   */
+  name: PropTypes.string,
+
+  /**
+   * SelectTimezone label
+   */
+  label: PropTypes.string,
+
+  /**
+   * SelectTimezone value
+   */
+  value: PropTypes.string,
+
+  /**
+   *  OnChaneg handler
+   */
+  onChange: PropTypes.func.isRequired,
+
+  /**
+   * Disable SelectTimezone
+   */
+  isDisabled: PropTypes.bool,
+
+  /**
+   * Keep SelectTimezone options searchable or non-searchable
+   */
+  isSearchable: PropTypes.bool,
+};
+
+SelectTimezone.defaultProps = {
+  isSearchable: true,
+};
---
+++ src/stories/SelectTimezone.stories.jsx
@@ -0,0 +1,85 @@
+import { SelectTimezone } from "../components/SelectTimezone";
+import { useState } from "react";
+
+export default {
+  title: "Example/SelectTimezone",
+  component: SelectTimezone,
+};
+
+export const Primary = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <SelectTimezone
+      name={"Custom timezone"}
+      label="Select Timezone"
+      value={value}
+      onChange={({ label, value }) => setValue(value)}
+      containerStyle={{ width: 300 }}
+    />
+  );
+};
+
+export const WithOptionFomatting = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <SelectTimezone
+      id="custom_timezone"
+      name={"Custom timezone"}
+      label="Select Timezone"
+      value={value}
+      onChange={({ label, value }) => setValue(value)}
+      defaultToSystemTimezone
+      containerStyle={{ width: 300 }}
+      labelStyles={{ color: "blue" }}
+      optionLabelFormat={(timzone) =>
+        `${timzone.name} (${timzone.abbreviation})`
+      }
+    />
+  );
+};
+
+export const WithSelectStyles = () => {
+  const [value, setValue] = useState();
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: "1px dotted pink",
+      color: state.isSelected ? "red" : "blue",
+      padding: 20,
+    }),
+  };
+  return (
+    <SelectTimezone
+      id="custom_timezone"
+      name={"Custom timezone"}
+      label="Select Timezone"
+      value={value}
+      onChange={({ label, value }) => setValue(value)}
+      defaultToSystemTimezone
+      containerStyle={{ width: 300 }}
+      selectStyles={customStyles}
+    />
+  );
+};
+
+export const WithLabelStyles = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <SelectTimezone
+      id="custom_timezone"
+      name={"Custom timezone"}
+      label="Select Timezone 😀"
+      value={value}
+      onChange={({ label, value }) => setValue(value)}
+      defaultToSystemTimezone
+      containerStyle={{ width: 300 }}
+      labelStyles={{ color: "blue" }}
+      optionLabelFormat={(timzone) =>
+        `${timzone.name} - ${timzone.abbreviation}`
+      }
+    />
+  );
+};
